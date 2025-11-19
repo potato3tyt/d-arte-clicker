dc501177cec09965597eff1d1eefcdd7c58223b1
potato3tyt <phcoutinhof@gmail.com>
2025-11-19 11:04:50 +0000

    HTML Linha 39: Juntei os nomes dos upgrades e os preços, também adicionei o que o upgrade faz
    CSS
    Linha 119: Mudei ligeiramente a animação do botão de cookies, para que ele desca ao passar o mouse por cima
    
    Java
    Linha 9: Adicionei uma constante "cps" (cliques por segundo)
    Linha 30: Adicionei a constante "upgrades por segundo"
    Linha 39: adicionei um evento que supostamente compra o upgrade do botão

--- java/script.js
+++ java/script.js
@@ -2,9 +2,12 @@
 let moedas = 0;
 let contagem = 0;
 
+
 //constantes do código
 const botao = document.getElementById("cookie-button");
 const almas = document.getElementById("cookie-count");
+const cps = document.getElementById("cps");
+
 
 //addeventlistener para adicionar a função de capturar almas para o botão
 botao.addEventListener("click", function() {
@@ -22,4 +25,25 @@ botao.addEventListener("click", function() {
     else{ 
         document.getElementById("bonus").textContent = "";
     }
+});
+
+const upgradesporsegundo = [
+    { id: "ASacerdotisa", custo: 10, cps: 1 },
+    { id: "ALua", custo: 600, cps: 5 },
+    { id: "OMago", custo: 1500, cps: 10 },
+    { id: "OLouco", custo: 4000, cps: 120 },
+    { id: "ODiabo", custo: 9000, cps: 50 },
+    { id: "OHierofante", custo: 19000, cps: 70 },
+];
+
+ASacerdotisa.addEventListener("click", function() {
+    console.log(cps);
+    if (moedas >= upgradesporsegundo.custo) {
+        cps = cps + upgradesporsegundo.cps;
+        moedas = moedas - upgrade.custo;
+        document.getElementById("cookie-count").textContent = moedas;
+        document.getElementById("cps").textContent = cps + " cps";
+    }
+    
+    console.log(upgradesporsegundo);
 });
